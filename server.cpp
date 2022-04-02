bba72420b5e64781f65bff46bcda45a4bd418122
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-04-02 12:18:30 -0700

    Linux Server Socket Error

--- server.cpp
+++ server.cpp
@@ -4,8 +4,11 @@
 #include <unistd.h>//file manage header
 #include <arpa/inet.h>
 #include <sys/socket.h>//socket programming lib
-
-void Error_handle(char *err_msg){//Error function
+const char *Socket_Err = "Socket Error!";
+const char *Bind_Err = "Bind Error!";
+const char *Listen_Err = "Listen Error!";
+const char *Accept_Err = "Accept Error!";
+void Error_handle(const char *err_msg){//Error function
 	fputs(err_msg,stderr);
 	fputc('\n',stderr);
 	exit(1);
@@ -23,7 +26,7 @@ int main(){
 	int port = 10000;
 	//Create Server Socket TCP/IP Protocal
 	server_socket = socket(PF_INET,SOCK_STREAM,0);
-	if(server_socket == -1) Error_handle("Socket Error!");//when error occured
+	if(server_socket == -1) Error_handle(Socket_Err);//when error occured
 
 	memset(&server_addr,0,sizeof(server_addr));//init
 	server_addr.sin_family = AF_INET;//TCP/IP protocal
@@ -32,16 +35,16 @@ int main(){
 
 	//bind allocate server socket addr
 	int Bind_ret = bind(server_socket,(struct sockaddr*) &server_addr,sizeof(server_addr));
-	if(Bind_ret == -1) Error_handle("Bind Error!");
+	if(Bind_ret == -1) Error_handle(Bind_Err);
 
 	//for listen wait clinet request
 	int Listen_ret = listen(server_socket,10);
-	if(Listen_ret == -1) Error_handle("listen Error!");
+	if(Listen_ret == -1) Error_handle(Listen_Err);
 
 	//Accept for structure and function
 	unsigned int Client_addr_size = sizeof(client_addr);
 	int Accept_ret = accept(server_socket,(struct sockaddr*) &client_addr, &Client_addr_size);
-	if(Accept_ret == -1) Error_handle("Accept Error!");
+	if(Accept_ret == -1) Error_handle(Accept_Err);
 
 	//send msg , contain rec msg string data
 	write(client_socket,Sendmsg,sizeof(Sendmsg));
